340d69a6ff47155255e1ddbff85a7d301813c17a
Jurijs Kolomijecs <jurikolo@yandex.com>
2017-01-11 22:53:05 +0200

    Added functionality to ressurrect hero

--- src/main/java/me/jurikolo/talekeepalive/Application.java
+++ src/main/java/me/jurikolo/talekeepalive/Application.java
@@ -37,35 +37,70 @@ public class Application {
 
             log.info("Execute GET request in order to fetch CSRFToken");
             ResponseEntity<Object> responseEntity = restTemplate.getForEntity(
-                    "http://the-tale.org/api/info?api_version=1.0&api_client=jurikolo-1", Object.class);
+                    "http://the-tale.org/game/api/info?api_version=1.6&api_client=jurikolo-1&account=10745", Object.class);
 
-            String sessionid = "";
-            String csrftoken = "";
-            for (String cookie: responseEntity.getHeaders().get("Set-Cookie")) {
-                if (cookie.startsWith("sessionid=")) {
-                    sessionid = cookie.substring(0, cookie.indexOf(";"));
+            String body = responseEntity.getBody().toString();
+            log.info(body);
+            if (body.contains("alive=true")) {
+                log.info("Hero is alive");
+            } else {
+                String sessionid = "";
+                String csrftoken = "";
+                for (String cookie : responseEntity.getHeaders().get("Set-Cookie")) {
+                    if (cookie.startsWith("sessionid=")) {
+                        sessionid = cookie.substring(0, cookie.indexOf(";"));
+                    }
+                    if (cookie.startsWith("csrftoken=")) {
+                        csrftoken = cookie.substring(0, cookie.indexOf(";"));
+                    }
                 }
-                if (cookie.startsWith("csrftoken=")) {
-                    csrftoken = cookie.substring(0, cookie.indexOf(";"));
+
+                log.info("Session id: " + sessionid);
+                log.info("CSRF token: " + csrftoken);
+
+                log.info("Now it's time to authorize");
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+                headers.add("Cookie", csrftoken);
+                headers.add("X-CSRFToken", csrftoken.replace("csrftoken=", ""));
+
+                MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+                map.add("email", "jurikolo@yandex.com");
+                map.add("password", "Привет, Андрей!");
+                //map.add("remember", "false");
+
+                HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+
+                ResponseEntity<String> response = restTemplate.postForEntity("http://the-tale.org/accounts/auth/api/login?api_version=1.0&api_client=jurikolo-1", request, String.class);
+                log.info(response.toString());
+
+                for (String cookie : response.getHeaders().get("Set-Cookie")) {
+                    if (cookie.startsWith("sessionid=")) {
+                        sessionid = cookie.substring(0, cookie.indexOf(";"));
+                    }
+                    if (cookie.startsWith("csrftoken=")) {
+                        csrftoken = cookie.substring(0, cookie.indexOf(";"));
+                    }
                 }
+                log.info("Session id: " + sessionid);
+                log.info("CSRF token: " + csrftoken);
+
+                HttpHeaders headers2 = new HttpHeaders();
+                headers2.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+                headers.add("Cookie", sessionid);
+                headers2.add("Cookie", csrftoken);
+                headers2.add("X-CSRFToken", csrftoken.replace("csrftoken=", ""));
+
+                MultiValueMap<String, String> map2 = new LinkedMultiValueMap<String, String>();
+                map2.add("email", "jurikolo@yandex.com");
+                map2.add("password", "66PAtEb6lzCJQJc8");
+
+                HttpEntity<MultiValueMap<String, String>> request2 = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+
+                ResponseEntity<String> response2 = restTemplate.postForEntity("http://the-tale.org/game/abilities/help/api/use?api_version=1.0&account=10745&api_client=jurikolo-1", request2, String.class);
+                log.info(response2.toString());
+
             }
-
-            log.info("Now it's time to authorize");
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-            //headers.add("Cookie", sessionid);
-            headers.add("Cookie", csrftoken);
-            headers.add("X-CSRFToken", csrftoken.replace("csrftoken=",""));
-
-            MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
-            map.add("email", "jurikolo@yandex.com");
-            map.add("password", "Привет Андрей");
-            //map.add("remember", "false");
-
-            HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
-
-            ResponseEntity<String> response = restTemplate.postForEntity( "http://the-tale.org/accounts/auth/api/login?api_version=1.0&api_client=jurikolo-1", request , String.class );
-            log.info(response.toString());
         };
     }
 }
